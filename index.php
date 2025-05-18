5f116a36a32e7272f956b766e26f08754f7ee652
suisrael <88337104+suisrael@users.noreply.github.com>
2025-05-18 10:27:59 -0400

    version and font changes

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 /*
 Plugin Name: Featured News Block
-Version: 1.0
-Author: Chiranjit Das
+Version: 1.1
 */
 include_once(plugin_dir_path(__FILE__) . 'includes/custom-api-functions.php');
 include_once(plugin_dir_path(__FILE__) . 'includes/latest-news-block.php');
